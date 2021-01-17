6a6b468ca7c65d64f40baaa21ff6d305292d85f8
Todd <todd@bocacode.com>
2021-01-17 13:10:51 -0500

    allow coffee to be passed via command line

--- src/coffee.js
+++ src/coffee.js
@@ -1,4 +1,4 @@
 const { getRecipe, printRecipe } = require('./helpers')
-const myCoffee = 'Latte'
+const myCoffee = process.argv[2] || 'Latte'
 const myRecipe = getRecipe(myCoffee)
 printRecipe(myCoffee, myRecipe)
